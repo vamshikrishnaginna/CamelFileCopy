e215fdd68936256c023bbadc3d18c2b3bc484d27
vamshi <vamshiginna1606@gmail.com>
2020-01-07 12:55:55 +0530

    1.Initial Comment

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class HelloWorld {
+    public static void main(String args[])
+
+    {
+        CamelContext context=new DefaultCamelContext();
+        try {
+            context.addRoutes(new RoutesBuilder() {
+                public void addRoutesToCamelContext(CamelContext context) throws Exception {
+                    System.out.println("Hello world!");
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
